67fbbea187f87ded607be6c5ce38eacbc9c5f70e
jonatakuzi <jonatakuzi@gmail.com>
2025-03-19 02:08:58 +0000

    website/src/app/layout.tsx

---
+++ website/src/app/page.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '@/theme';
+import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
+import ModeSwitch from '@/components/ModeSwitch';
+
+
+export default function RootLayout(props: { children?: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <InitColorSchemeScript attribute="class" />
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <ModeSwitch />
+            {/* Render additional children */}
+            {props.children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
